99e1190e45db2c465397c7d64d03ae2d8c1cb40d
pmkaboo <pm.kaboo@gmail.com>
2017-09-21 14:23:22 +0200

    add vat price sum, min, max

--- products.rb
+++ products.rb
@@ -16,11 +16,23 @@ class Products
 		@collection.map(&:price).sum
 	end
 
+	def vat_price_sum
+		@collection.map(&:vat_price).sum
+	end
+
 	def price_min
 		@collection.group_by(&:price).min.last
 	end
 
+	def vat_price_min
+		@collection.group_by(&:vat_price).min.last
+	end
+
 	def price_max
 		@collection.group_by(&:price).max.last
+	end
+
+	def vat_price_max
+		@collection.group_by(&:vat_price).max.last
 	end
 end
--- products_spec.rb
+++ products_spec.rb
@@ -4,11 +4,11 @@ require './products'
 
 describe Products do
 	before do
-		@product1 = Product.new 'p1', 1
-		@product2 = Product.new 'p2', 2
-		@product3 = Product.new 'p3', 3
-		@product4 = Product.new 'p4', 1
-		@product5 = Product.new 'p5', 2
+		@product1 = Product.new 'p1', 100, 10
+		@product2 = Product.new 'p2', 200, 20
+		@product3 = Product.new 'p3', 300, 30
+		@product4 = Product.new 'p4', 100, 40
+		@product5 = Product.new 'p5', 200, 50
 		@products = Products.new @product1, @product2
 	end
 
@@ -30,9 +30,17 @@ describe Products do
 
 	describe '#price_sum' do
 		it 'sums prices of products in the collection' do
-			@products.price_sum.must_equal 3
+			@products.price_sum.must_equal 300
 			@products << @product3
-			@products.price_sum.must_equal 6
+			@products.price_sum.must_equal 600
+		end
+	end
+
+	describe '#vat_price_sum' do
+		it 'sums vat prices of products in the collection' do
+			@products.vat_price_sum.must_equal 350
+			@products << @product3
+			@products.vat_price_sum.must_equal 740
 		end
 	end
 
@@ -45,12 +53,30 @@ describe Products do
 		end
 	end
 
+	describe '#vat_price_min' do
+		it 'returns collection of products with min vat price' do
+			@products << @product4
+			@products.vat_price_min.must_include @product1
+			@products.vat_price_min.wont_include @product4
+			@products.vat_price_min.wont_include @product2
+		end
+	end
+
 	describe '#price_max' do
 		it 'returns collection of products with max price' do
 			@products << @product5
 			@products.price_max.must_include @product2
 			@products.price_max.must_include @product5
 			@products.price_max.wont_include @product1
+		end
+	end
+
+	describe '#vat_price_max' do
+		it 'returns collection of products with max vat price' do
+			@products << @product5
+			@products.vat_price_max.wont_include @product2
+			@products.vat_price_max.must_include @product5
+			@products.vat_price_max.wont_include @product1
 		end
 	end
 end
