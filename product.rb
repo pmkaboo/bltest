d261d1b060e04f79ed89bb1872bcd0a3b74bf075
pmkaboo <pm.kaboo@gmail.com>
2017-09-21 15:34:48 +0200

    add sub products

--- product.rb
+++ product.rb
@@ -1,13 +1,34 @@
 class Product
 	attr_accessor :name, :price, :vat
+	attr_reader :children
 
-	def initialize name, price, vat
+	def initialize name, price, vat, parent = nil
 		@name = name
 		@price = price.to_f
 		@vat = vat.to_f
+
+		@children = []
+		parent << self if parent
 	end
 
 	def vat_price
 		price + price * (vat / 100)
+	end
+
+	def total_price vat = false
+		price_type = vat ? 'vat_price' : 'price'
+		send(price_type) + descendants_price(self, price_type)
+	end
+
+	def << child
+		@children << child
+		self
+	end
+
+	private
+	def descendants_price product, price_type
+		product.children.map do |child|
+			child.send(price_type) + descendants_price(child, price_type)
+		end.sum
 	end
 end
--- product_spec.rb
+++ product_spec.rb
@@ -3,7 +3,7 @@ require './product'
 
 describe Product do
 	before do
-		@product = Product.new 'name', 123, 20
+		@product = Product.new 'name', 100, 20
 	end
 
 	describe '#name' do
@@ -14,7 +14,7 @@ describe Product do
 
 	describe '#price' do
 		it 'returns price of the product' do
-			@product.price.must_equal 123
+			@product.price.must_equal 100
 		end
 	end
 
@@ -26,7 +26,35 @@ describe Product do
 
 	describe '#vat_price' do
 		it 'returns vat price of the product' do
-			@product.vat_price.must_equal 147.6
+			@product.vat_price.must_equal 120
+		end
+	end
+
+	describe '#<<' do
+		it 'adds sub product to the product' do
+			product2 = Product.new 'name2', 1, 1
+			@product << product2
+			@product.children.must_include product2
+			product3 = Product.new 'name3', 1, 1, @product
+			@product.children.must_include product3
+		end
+	end
+
+	describe '#total_price' do
+		it 'sums prices of itself and its descendants' do
+			product2 = Product.new 'name2', 100, 10, @product
+			product3 = Product.new 'name3', 100, 10, @product
+			product4 = Product.new 'name3', 100, 10, product2
+			product5 = Product.new 'name3', 100, 10, product4
+			@product.total_price.must_equal 500
+		end
+
+		it 'sums vat prices of itself and its descendants' do
+			product2 = Product.new 'name2', 100, 10, @product
+			product3 = Product.new 'name3', 100, 10, @product
+			product4 = Product.new 'name3', 100, 10, product2
+			product5 = Product.new 'name3', 100, 10, product4
+			@product.total_price(true).must_equal 560
 		end
 	end
 end
