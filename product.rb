0543e3043fb1dbc30dc35fc97168ec390dcef03d
pmkaboo <pm.kaboo@gmail.com>
2017-09-22 12:05:40 +0200

    convert price to bigdecimal, add to_s

--- product.rb
+++ product.rb
@@ -1,8 +1,17 @@
+require 'bigdecimal'
+require 'bigdecimal/util'
+
 class Product
 	attr_accessor :name, :price
 
 	def initialize name, price
 		@name = name
-		@price = price
+		@price = price.to_d
 	end
+
+	def to_s
+		# regex truncate to always display two decimals
+		name + ', ' + (price.to_s('F') + '0')[/.*\..{2}/]
+	end
+
 end
--- product_spec.rb
+++ product_spec.rb
@@ -17,4 +17,12 @@ describe Product do
 			@product.price.must_equal 123
 		end
 	end
+
+	describe '#to_s' do
+		it 'returns name and price with two decimals' do
+			@product.to_s.must_equal 'name, 123.00'
+			product2 = Product.new 'name2', 19.999
+			product2.to_s.must_equal 'name2, 19.99'
+		end
+	end
 end
